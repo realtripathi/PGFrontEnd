ad620e728890097acad1a5f1db1df4c3fb5c0a3f
Bhawna-git <kumaribhawna0306@gmail.com>
2020-10-27 23:30:26 +0530

    Added student.ts and student-register component

--- src/app/student-register/student-register.component.ts
+++ src/app/student-register/student-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from '../student'
 
 @Component({
   selector: 'app-student-register',
@@ -11,5 +12,9 @@ export class StudentRegisterComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  studentModel= new Student("","","","","","","","","","","","","");
+
+  studentStates = ['Andhra Pradesh','Arunachal Pradesh','Assam','Bihar','Chhattisgarh','Goa','Gujarat','Haryana','Himachal Pradesh','Jammu and Kashmir','Jharkhand','Karnataka','Kerala','Madhya Pradesh','Maharashtra','Manipur','Meghalaya','Mizoram','Nagaland','Odisha','Punjab','Rajasthan','Sikkim','Tamil Nadu','Telangana','Tripura','Uttarakhand','Uttar Pradesh','West Bengal','Andaman and Nicobar Islands','Chandigarh','Dadra and Nagar Haveli','Daman and Diu','Delhi','Lakshadweep','Puducherry'];
 
 }
---
+++ src/app/student.ts
@@ -0,0 +1,17 @@
+export class Student {
+    constructor(
+      public studentAadharNumber: String,
+      public studentName: String,
+      public studentGender:String,
+      public studentDateOfBirth: String,
+      public studentMobileNumber:String,
+      public studentEmail:String,
+      public studentStateOfDomicile:String,
+      public studentDistrict:String,
+      public studentBankName:String,
+      public studentBankAccountNumber:String,
+      public studentIfscCode:String,
+      public studentPassword:String,
+      public studentConfirmPassword: String
+    ) { }
+  }
