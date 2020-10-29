ecb011d0fc34214e559d193b140832e209766387
Bhawna-git <kumaribhawna0306@gmail.com>
2020-10-29 20:57:10 +0530

    modified nodal.ts and nodal-reg

--- src/app/nodal-register/nodal-register.component.ts
+++ src/app/nodal-register/nodal-register.component.ts
@@ -15,7 +15,7 @@ export class NodalRegisterComponent implements OnInit {
 
   nodalStates = ['Andhra Pradesh','Arunachal Pradesh','Assam','Bihar','Chhattisgarh','Goa','Gujarat','Haryana','Himachal Pradesh','Jammu and Kashmir','Jharkhand','Karnataka','Kerala','Madhya Pradesh','Maharashtra','Manipur','Meghalaya','Mizoram','Nagaland','Odisha','Punjab','Rajasthan','Sikkim','Tamil Nadu','Telangana','Tripura','Uttarakhand','Uttar Pradesh','West Bengal','Andaman and Nicobar Islands','Chandigarh','Dadra and Nagar Haveli','Daman and Diu','Delhi','Lakshadweep','Puducherry'];
 
-  NodalModel= new Nodal("","","","","",true);
+  nodalModel= new Nodal(null,null,null,null,null,true);
 
  
 
