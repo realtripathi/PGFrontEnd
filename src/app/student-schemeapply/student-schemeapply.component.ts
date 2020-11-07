66a559a2b60333cf733990dbb1f3f180bf460a4c
realtripathi <shubham.tripathi676@gmail.com>
2020-11-07 17:28:10 +0530

    confirmpassword and applyforscheme

--- src/app/student-schemeapply/student-schemeapply.component.ts
+++ src/app/student-schemeapply/student-schemeapply.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ScholarshipForm } from "../scholarship-form";
+import { StudentService } from '../student.service'
 
 @Component({
   selector: 'app-student-schemeapply',
@@ -9,12 +11,30 @@ import { ScholarshipForm } from "../scholarship-form";
 export class StudentSchemeapplyComponent implements OnInit {
 
 
-  scholarshipFormModel=new ScholarshipForm(null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,false);
+  scholarshipFormModel=new ScholarshipForm();
+
+  instituteId: number = 1006;
+
+  studentId: number = 2006;
+  schemeId: number =3006;
   
 
-  constructor() { }
+  constructor(private studentService:StudentService,private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  applyScheme(){
+    console.log(this.scholarshipFormModel);
+    this.studentService.applyScheme(this.scholarshipFormModel,this.instituteId,this.schemeId,this.scholarshipFormModel.aadharNumber).subscribe(data => {
+     //alert(JSON.stringify(data));
+     if(data.status == 'SUCCESS') {
+       this.router.navigate(['studentProfile']);
+     }
+     else {
+       //this.router.navigate(['error']);
+     }
+   })
   }
 
 }
--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { LoginStatus } from './login-status';
+import { ScholarshipForm } from './scholarship-form';
 import { Status } from './status';
 import { Student } from './student';
 import { StudentLogin } from './student-login';
@@ -11,15 +12,25 @@ import { StudentLogin } from './student-login';
 })
 export class StudentService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  register(student:Student,instiute_id:Number): Observable<Status> {
-    let url = 'http://localhost:8080/registerStudent?institute_id='+instiute_id;
+  register(student: Student, instiute_id: Number): Observable<Status> {
+    let url = 'http://localhost:8080/registerStudent?institute_id=' + instiute_id;
     return this.http.post<Status>(url, student);
   }
 
   login(login: StudentLogin): Observable<LoginStatus> {
     let url = 'http://localhost:8080/studentLogin';
     return this.http.post<LoginStatus>(url, login);
+  }
+
+  applyScheme(form : ScholarshipForm, instituteId: Number, schemeId: Number, studentId: Number) {
+
+    // let params = new HttpParams()
+    //   .set('intituteId', instituteId)
+    //   .set('schemeId', schemeId)
+    //   .set('studentId', studentId);
+    let url = 'http://localhost:8080/applyForScheme?instiuteId=' + instituteId + '&schemeId=' + schemeId +'&studentId=' +studentId;
+    return this.http.post<Status>(url, form);
   }
 }
