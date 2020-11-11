aee3c72bffd1e40854f16ca248e70bcd63625f17
Vilesh123 <vileshagrawal123@gmail.comm>
2020-11-11 09:47:45 +0530

    Verify components updated

--- src/app/nodal-institute-verify/nodal-institute-verify.component.ts
+++ src/app/nodal-institute-verify/nodal-institute-verify.component.ts
@@ -20,6 +20,7 @@ export class NodalInstituteVerifyComponent implements OnInit {
     let id = parseInt(this.route.snapshot.paramMap.get('id'));
     this.instituteId = id;
     this.schemeService.showInstitute(this.instituteId).subscribe(data => this.instituteModel = data);
+    alert(JSON.stringify(this.instituteModel));
   }
 
   approveInstitute(){
@@ -32,3 +33,4 @@ export class NodalInstituteVerifyComponent implements OnInit {
 
 
 }
+
